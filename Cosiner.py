b8bfb097aa71866b6e6f9ab68bff9192fb06f229
Gavs <poOdrag0n69@gmail.com>
2022-01-31 20:14:43 +0800

    Update 3

--- Cosiner.py
+++ Cosiner.py
@@ -5,8 +5,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 pd.set_option('display.max_columns', None)
 
-#csv file name to be cosined
-in_csv = './data/intermediate_data/processed_games_for_content-based.csv'
+# csv file name to be cosined
+in_csv = './data/intermediate_data/steam_games5000.csv'
 
 # Read the csv
 dataGames = pd.read_csv(in_csv)
@@ -19,10 +19,10 @@ dataGames['genre'] = dataGames['genre'].fillna('')
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(dataGames['genre'])
 cosine_sim_matrix = cosine_similarity(
-      count_matrix, count_matrix)
+    count_matrix, count_matrix)
 
 # Save the output
-np.savetxt("./Rating/cosined_data_small.csv", cosine_sim_matrix, delimiter = ",")
+np.savetxt("./OUTPUT/cosined_data.csv", cosine_sim_matrix, delimiter=",")
 # cosine_sim_matrix.to_csv(
 #       out_csv,
 #       index=False,
